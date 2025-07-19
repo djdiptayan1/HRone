14789987a7668acd2f13b3e7fbf5d4cf2ed49983
Diptayan Jash <dj2037@srmist.edu.in>
2025-07-19 12:44:24 +0530

    Refactor product routes to include JWT authentication dependency

--- routes/product_routes.py
+++ routes/product_routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
 from controllers.product_controller import (
     create_new_product,
     list_products,
@@ -6,29 +6,45 @@ from controllers.product_controller import (
     edit_product,
 )
 from models.product_model import ProductCreate
+from middleware.auth import JWTBearer
 
 router = APIRouter()
+jwt_bearer = JWTBearer()
 
 
 @router.post("/products", status_code=201)
-async def create_product_endpoint(product: ProductCreate):
+async def create_product_endpoint(
+    product: ProductCreate,
+    # token: str = Depends(jwt_bearer)
+):
     return create_new_product(product)
 
 
 @router.get("/products")
 async def get_products_endpoint(
-    name: str = None, size: str = None, limit: int = 10, offset: int = 0
+    name: str = None,
+    size: str = None,
+    limit: int = 10,
+    offset: int = 0,
+    # token: str = Depends(jwt_bearer),
 ):
     return list_products(name, size, limit, offset)
 
 
 @router.delete("/products/{product_id}", status_code=204)
-async def delete_product_endpoint(product_id: str):
+async def delete_product_endpoint(
+    product_id: str,
+    #   token: str = Depends(jwt_bearer)
+):
     return delete_product(product_id)
 
 
 @router.put("/products/{product_id}", status_code=202)
-async def edit_product_endpoint(product_id: str, product: ProductCreate):
+async def edit_product_endpoint(
+    product_id: str,
+    product: ProductCreate,
+    # token: str = Depends(jwt_bearer)
+):
     updated_product = edit_product(product_id, product)
     if not updated_product:
         raise HTTPException(status_code=404, detail="Product not found")
